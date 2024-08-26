4e56decc63f620af0f2a74ed2b7c6bdd300934e5
gabriel <gabrielvalino25@hotmail.com>
2024-08-26 19:21:04 -0300

    Update error pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -20,13 +21,16 @@ export const router = createBrowserRouter([
   },
 
   {
-    path: '/sign-in',
+    path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
+
   {
-    path: '/sign-up',
-    element: <AuthLayout />,
-    children: [{ path: '/sign-up', element: <SignUp /> }],
+    path: '*',
+    element: <NotFound />,
   },
 ])
